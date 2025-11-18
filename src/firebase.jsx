88a325ca64d7f94fc65a62ca811ed36ac90e07b4
networkSorcerer <gigattfyhig@gmail.com>
2025-11-18 16:27:39 +0900

    firebase realtime db

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,15 +1,14 @@
 // src/firebase.jsx
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAiF6AukDhkj1hbaoKK6Iw9imrmWJFnNFQ",
-  authDomain: "fir-78f97.firebaseapp.com",
-  databaseURL: "https://fir-78f97-default-rtdb.firebaseio.com",
-  projectId: "fir-78f97",
-  storageBucket: "fir-78f97.firebasestorage.app",
-  messagingSenderId: "315472783806",
-  appId: "1:315472783806:web:1b6d0ba68bac5bc38431ef",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
